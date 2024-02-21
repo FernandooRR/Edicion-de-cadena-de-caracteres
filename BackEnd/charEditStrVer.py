c61b1a0312621e260fee4d4b317e0639f59474c3
FernandooRR <131893714+FernandooRR@users.noreply.github.com>
2024-02-20 20:23:36 -0600

    Update charEditStrVer.py
    Similar a la version de chunks, ya realiza las combinaciones pero no de manera precisa cuando el conjunto supera a los 2 elementos

--- BackEnd/charEditStrVer.py
+++ BackEnd/charEditStrVer.py
@@ -14,25 +14,43 @@ def changeCsvString(data, chunkRange, jump):
   for i in range(0, len(string), jump):
       change = False
       chunk = list(string[i:min(i + chunkRange, len(string))])  # Obtenemos el chunk actual
+      changeRefs = []
       window = []
 
       # Verificamos si hay elementos en el chunk que necesitan ser cambiados
       for j, char in enumerate(chunk):
           chunk_index = i + j
+          nuStr = list(string)
           for ref in refList:
               if ref[0] == chunk_index:  # Comparamos el índice del chunk con la posición en refList
                   if len(ref) > 1:
-                      volatileStr = list(string) # Creamos una copia del string original
-                      volatileStr[j] = ref[2]  # Reemplazamos el carácter en el string
-                      window.append("".join(volatileStr))  # Almacenamos la nueva versión del string en la ventana
+                      modStr = list(string) # Creamos una copia del string original
+                      modStr[j] = ref[2]  # Reemplazamos el carácter en el string
+                      nuStr[j] = ref[2]
+                      changeRefs.append(ref)
+                      window.append("".join(modStr))  # Almacenamos la nueva versión del string en la ventana
                       change = True  # Indicamos que hubo un cambio
+      if len(changeRefs) > 2:
+        refComb = []
+        for r in range(1,len(changeRefs)):
+          refComb.extend(itools.combinations(changeRefs, r))
+        for ref in range(len(changeRefs), len(refComb)):
+          print(refComb[ref])
+          refs = refComb[ref]
+          combStr = list(string)
+          for it in refs:
+            for j, char in enumerate(chunk):
+              chunkIndex = i + j
+              if it[0] == chunkIndex:
+                combStr[j] = it[2]
+          window.append("".join(combStr))
 
-      if len(window) > 1: #Evalua si hay mas de un cambio registrado en la ventana
-        combinaciones = []
-        for r in range(2, len(window) + 1):
-          combinaciones.extend(itools.combinations(window, r))
-        for combinacion in combinaciones:
-          window.extend(combinacion)
+      if change:
+        if len(changeRefs) > 1:
+          window.append("".join(nuStr))
+        windowList.append(window)
+
+  return windowList
 
       # Imprimimos la ventana solo si se realizaron cambios en el chunk
       if change:
