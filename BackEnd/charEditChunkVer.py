c39b13cf7cce980e1c196af0ae0a73b3249c89cb
FernandooRR <131893714+FernandooRR@users.noreply.github.com>
2024-02-20 20:22:13 -0600

    Update charEditChunkVer.py
    Ya realiza las combinaciones, más sin embargo no llegan a ser precisas cuando el conjunto de cambios en la ventana supera a 2, se investigará, fuera de eso funciona de manera precisa

--- BackEnd/charEditChunkVer.py
+++ BackEnd/charEditChunkVer.py
@@ -14,29 +14,42 @@ def changeCsvString(data, chunkRange, jump):
   for i in range(0, len(string), jump):
       change = False
       chunk = list(string[i:min(i + chunkRange, len(string))])  # Obtenemos el chunk actual
+      nuChunk = chunk.copy()
       window = []
-
+      changeRefs = []
+      chunkIndex = 0
       # Verificamos si hay elementos en el chunk que necesitan ser cambiados
       for j, char in enumerate(chunk):
-          chunk_index = i + j
+          chunkIndex = i + j
           for ref in refList:
-              if ref[0] == chunk_index:  # Comparamos el índice del chunk con la posición en refList
+              if ref[0] == chunkIndex:  # Comparamos el índice del chunk con la posición en refList
                   if len(ref) > 1:
                       modChunk = chunk[:]  # Creamos una copia del chunk original
                       modChunk[j] = ref[2]  # Reemplazamos el carácter en el chunk
+                      nuChunk[j] = ref[2]
+                      changeRefs.append(ref)
                       window.append("".join(modChunk))  # Almacenamos la nueva versión del chunk en la ventana
                       change = True  # Indicamos que hubo un cambio
+      
+      if len(changeRefs) > 2:
+        refComb = []
+        for r in range(1,len(changeRefs)):
+          refComb.extend(itools.combinations(changeRefs, r))
+        for ref in range(len(changeRefs), len(refComb)):
+          print(refComb[ref])
+          refs = refComb[ref]
+          combChunk = chunk
+          for it in refs:
+            for j, char in enumerate(chunk):
+              chunkIndex = i + j
+              if it[0] == chunkIndex:
+                combChunk[j] = it[2]
+          window.append("".join(combChunk))
 
-      if len(window) > 1:
-        combinaciones = []
-        for r in range(2, len(window) + 1):
-          combinaciones.extend(itools.combinations(window, r))
-        for combinacion in combinaciones:
-          window.extend(combinacion)
-      # Imprimimos la ventana solo si se realizaron cambios en el chunk
       if change:
+        if len(changeRefs) > 1:
+          window.append("".join(nuChunk))
         windowList.append(window)
 
   return windowList
-
 
